4909345616b09669ae3d272ba22b0439f57fde6d
Jaime Jimenez <jaimejimenez99@hotmail.com>
2022-11-16 18:46:35 +0100

    login advanced

---
+++ config.js
@@ -0,0 +1,14 @@
+"use strict"
+
+module.exports = {
+
+    mysqlConfig: {
+        host: "localhost",
+        user:"root",
+        password: "",
+        database: "UCM-CAU"
+    },
+
+    port: 3306
+    
+};
